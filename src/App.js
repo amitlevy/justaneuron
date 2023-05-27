adcfdc6b6eece686f68ceb400613df38fdf934b3
Amit Levy <amitlevy49@gmail.com>
2023-05-27 16:49:42 +0300

    finished skeleton of PoC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactFlow from 'reactflow';
+import 'reactflow/dist/style.css'
+
+function createInputNode(id, x, y) {
+  return {
+    id,
+    position: { x, y },
+    data: { label: id },
+    sourcePosition: 'right',
+    type: 'input'
+  };
+}
+
+function createOperatorNode(id, x, y, label = null, sourcePosition = 'right', targetPosition = 'left', width = 50) {
+  return {
+    id,
+    position: { x, y },
+    data: { label: label || id },
+    sourcePosition,
+    targetPosition,
+    style: { width },
+  };
+}
+
+function createEdge(source, target, animated = true) {
+  return { source, target, animated };
+}
+
+const nodes = [
+  createInputNode('x1', 0, 0),
+  createInputNode('w1', 0, 100),
+  createOperatorNode('mult1', 300, 50, '*'),
+  createInputNode('x2', 0, 300),
+  createInputNode('w2', 0, 400),
+  createOperatorNode('mult2', 300, 350, '*'),
+  createOperatorNode('add', 500, 200, '+'),
+  createOperatorNode('ReLU', 700, 200, 'ReLU'),
+];
+
+const edges = [
+  createEdge('x1', 'mult1'),
+  createEdge('w1', 'mult1'),
+  createEdge('x2', 'mult2'),
+  createEdge('w2', 'mult2'),
+  createEdge('mult1', 'add'),
+  createEdge('mult2', 'add'),
+  createEdge('add', 'ReLU'),
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow nodes={nodes} edges={edges} attributionPosition="bottom-left" fitView />
     </div>
   );
 }
