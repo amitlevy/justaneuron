a62dd36cb49719dc5734beda929cc3b355ea1aa4
Justin Harrison <jhar-dev@protonmail.com>
2023-06-02 12:14:41 -0700

    trace function

--- src/utils.js
+++ src/utils.js
@@ -25,8 +25,53 @@ function createEdge(source, target, animated = true) {
   return { source, target, animated };
 }
 
+// builds arrays for all nodes and edges in a graph
+function trace(root) {
+  const nodes = new Set();
+  const edges = new Set();
+
+  function build(v) {
+    if (!nodes.has(v)) {
+      const newNode = {
+        id: v.id,
+        position: { x: 0, y: 0 }, // this needs to be calculated separately
+        data: { label: v.id + ` | Data: ${v.data} | Grad: ${v.grad}` },
+        sourcePosition: 'right',
+        value: v,
+      };
+
+      nodes.add(v.op === '' ? {
+        ...newNode,
+        type: 'input',
+      } : {
+        ...newNode,
+        targetPosition: 'left',
+        style: { width: 200 },
+      });
+
+      for (const child of v.children) {
+        edges.add({
+          source: child.id,
+          target: v.id,
+          animated: true,
+        });
+
+        build(child);
+      }
+    }
+  }
+
+  build(root);
+
+  return {
+    nodes: Array.from(nodes),
+    edges: Array.from(edges),
+  }
+}
+
 export {
   createInputNode,
   createOperatorNode,
   createEdge,
+  trace,
 };
--- src/value.js
+++ src/value.js
@@ -1,69 +1,75 @@
+import { v4 as uuidv4 } from 'uuid';
+
 class Value {
-    constructor(data, children = [], op = '') {
-        this.data = data;
-        this.children = children;
-        this.op = op;
-        this._backward = function () {
-            return null;
-        };
-        this.grad = 0.0;
-    }
+  constructor(data, children = [], op = '') {
+    this.id = uuidv4();
+    this.data = data;
+    this.children = children;
+    this.op = op;
+    this._backward = function () {
+      return null;
+    };
 
-    add(other) {
-        const out = new Value(this.data + other.data, [this, other], '+');
+    this.grad = 0.0;
+  }
 
-        out._backward = () => {
-            this.grad += out.grad;
-            other.grad += out.grad;
-        };
+  add(other) {
+    const out = new Value(this.data + other.data, [this, other], '+');
 
-        return out;
-    }
+    out._backward = () => {
+      this.grad += out.grad;
+      other.grad += out.grad;
+    };
 
-    mul(other) {
-        const out = new Value(this.data * other.data, [this, other], '*');
+    return out;
+  }
 
-        out._backward = () => {
-            this.grad += other.data * out.grad;
-            other.grad += this.data * out.grad;
-        };
+  mul(other) {
+    const out = new Value(this.data * other.data, [this, other], '*');
 
-        return out;
-    }
+    out._backward = () => {
+      this.grad += other.data * out.grad;
+      other.grad += this.data * out.grad;
+    };
 
-    relu() {
-        const out = new Value(this.data < 0 ? 0.0 : this.data, [this], 'ReLU');
+    return out;
+  }
 
-        out._backward = () => {
-            this.grad += (out.data > 0.0 ? 1.0 : 0.0) * out.grad;
-        };
-        return out;
-    }
+  relu() {
+    const out = new Value(this.data < 0 ? 0.0 : this.data, [this], 'ReLU');
 
-    backward() {
-        const topo = [];
-        const visited = new Set();
-        const build_topo = function (v) {
-            if (!visited.has(v)) {
-                visited.add(v);
-                for (const child of v.children) {
-                    build_topo(child);
-                }
-                topo.push(v);
-            }
-        };
-        build_topo(this);
+    out._backward = () => {
+      this.grad += (out.data > 0.0 ? 1.0 : 0.0) * out.grad;
+    };
 
-        this.grad = 1;
-        topo
-            .slice()
-            .reverse()
-            .forEach(function (v) {
-                v._backward();
-            });
-    }
+    return out;
+  }
+
+  backward() {
+    const topo = [];
+    const visited = new Set();
+    const build_topo = function (v) {
+      if (!visited.has(v)) {
+        visited.add(v);
+        for (const child of v.children) {
+          build_topo(child);
+        }
+        topo.push(v);
+      }
+    };
+
+    build_topo(this);
+
+    this.grad = 1;
+    topo
+      .slice()
+      .reverse()
+      .forEach(function (v) {
+        v._backward();
+      });
+  }
 }
 
-module.exports = {
-    Value
+export {
+  Value
 };
