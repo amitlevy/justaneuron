727c9b030ea8a047f6eb27dd9db2cc04514a3da7
Amit Levy <amitlevy49@gmail.com>
2023-06-02 17:45:30 +0300

    backprop poc, input is from temporary increment button

--- src/App.js
+++ src/App.js
@@ -2,42 +2,35 @@ import './App.css';
 import React from 'react';
 import ReactFlow from 'reactflow';
 import 'reactflow/dist/style.css'
+import { Value } from './value';
+import { useState } from 'react';
 
-function createInputNode(id, x, y) {
+function createInputNode(id, x, y, value) {
   return {
     id,
     position: { x, y },
-    data: { label: id },
+    data: { label: id + ` | Data: ${value.data} | Grad: ${value.grad}` },
     sourcePosition: 'right',
-    type: 'input'
+    type: 'input',
+    value: value
   };
 }
 
-function createOperatorNode(id, x, y, label = null, sourcePosition = 'right', targetPosition = 'left', width = 50) {
+function createOperatorNode(id, x, y, label = null, value, width = 200, sourcePosition = 'right', targetPosition = 'left') {
   return {
     id,
     position: { x, y },
-    data: { label: label || id },
+    data: { label: label + ` | Data: ${value.data} | Grad: ${value.grad}` },
     sourcePosition,
     targetPosition,
     style: { width },
+    value: value
   };
 }
 
 function createEdge(source, target, animated = true) {
   return { source, target, animated };
 }
-
-const nodes = [
-  createInputNode('x1', 0, 0),
-  createInputNode('w1', 0, 100),
-  createOperatorNode('mult1', 300, 50, '*'),
-  createInputNode('x2', 0, 300),
-  createInputNode('w2', 0, 400),
-  createOperatorNode('mult2', 300, 350, '*'),
-  createOperatorNode('add', 500, 200, '+'),
-  createOperatorNode('ReLU', 700, 200, 'ReLU'),
-];
 
 const edges = [
   createEdge('x1', 'mult1'),
@@ -50,8 +43,31 @@ const edges = [
 ];
 
 function App() {
+  const [x1, setX1] = useState(3);
+  let x1_val = new Value(x1);
+  let x2_val = new Value(2);
+  let w1_val = new Value(4);
+  let w2_val = new Value(0);
+  let mult1_val = x1_val.mul(w1_val);
+  let mult2_val = x2_val.mul(w2_val);
+  let add_val = mult1_val.add(mult2_val);
+  let relu_val = add_val.relu();
+  relu_val.backward();
+
+  const nodes = [
+    createInputNode('x1', 0, 0, x1_val),
+    createInputNode('w1', 0, 100, w1_val),
+    createOperatorNode('mult1', 400, 50, 'w1*x1', mult1_val),
+    createInputNode('x2', 0, 300, x2_val),
+    createInputNode('w2', 0, 400, w2_val),
+    createOperatorNode('mult2', 400, 350, 'w2*x2', mult2_val),
+    createOperatorNode('add', 700, 200, 'm1+m2', add_val),
+    createOperatorNode('ReLU', 1000, 200, 'ReLU', relu_val),
+  ];
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+      <button onClick={() => setX1(x1+1)}>Increment x1</button>
       <ReactFlow nodes={nodes} edges={edges} attributionPosition="bottom-left" fitView />
     </div>
   );
---
+++ src/value.js
@@ -0,0 +1,69 @@
+class Value {
+    constructor(data, children = [], op = '') {
+        this.data = data;
+        this.children = children;
+        this.op = op;
+        this._backward = function () {
+            return null;
+        };
+        this.grad = 0.0;
+    }
+
+    add(other) {
+        const out = new Value(this.data + other.data, [this, other], '+');
+
+        out._backward = () => {
+            this.grad += out.grad;
+            other.grad += out.grad;
+        };
+
+        return out;
+    }
+
+    mul(other) {
+        const out = new Value(this.data * other.data, [this, other], '*');
+
+        out._backward = () => {
+            this.grad += other.data * out.grad;
+            other.grad += this.data * out.grad;
+        };
+
+        return out;
+    }
+
+    relu() {
+        const out = new Value(this.data < 0 ? 0.0 : this.data, [this], 'ReLU');
+
+        out._backward = () => {
+            this.grad += (out.data > 0.0 ? 1.0 : 0.0) * out.grad;
+        };
+        return out;
+    }
+
+    backward() {
+        const topo = [];
+        const visited = new Set();
+        const build_topo = function (v) {
+            if (!visited.has(v)) {
+                visited.add(v);
+                for (const child of v.children) {
+                    build_topo(child);
+                }
+                topo.push(v);
+            }
+        };
+        build_topo(this);
+
+        this.grad = 1;
+        topo
+            .slice()
+            .reverse()
+            .forEach(function (v) {
+                v._backward();
+            });
+    }
+}
+
+module.exports = {
+    Value
+};
