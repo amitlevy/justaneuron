983f5dbd2d2ba7a2c275054cdf35df864814ccb0
Amit Levy <amitlevy49@gmail.com>
2023-06-12 21:38:08 +0300

    adding bias node to defaults

--- src/defaults.js
+++ src/defaults.js
@@ -5,10 +5,12 @@ let x1_val = new Value(2);
 let x2_val = new Value(2);
 let w1_val = new Value(4);
 let w2_val = new Value(0);
+let b_val = new Value(30);
 let mult1_val = x1_val.mul(w1_val);
 let mult2_val = x2_val.mul(w2_val);
 let add_val = mult1_val.add(mult2_val);
-let relu_val = add_val.relu();
+let add2_val = add_val.add(b_val);
+let relu_val = add2_val.relu();
 relu_val.backward();
 
 const { nodes, edges } = trace(relu_val);
